d374dc0329f1077c6afec3d5c63b33b68748aa4d
kefflen <kefflenjava@hotmail.com>
2020-08-24 12:13:42 +0000

    24-08

---
+++ vetores.js
@@ -0,0 +1,13 @@
+
+
+let a = ["a", "b", "c", "t"]
+let o = {"T": 2, "B": 4, "C": 6}
+
+for (let i in o) {
+    console.log(i)
+}
+
+for (i in a) {
+    console.log(i)
+}
+pessoa3["data de nascimento"]
